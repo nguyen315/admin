2316a8d504721af48b68c83f16aa0d5a7b3c1e98
nhanlinh1478 <hominhnguyen315@gmail.com>
2020-12-08 20:07:26 +0700

    search and group by category

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,25 +2,58 @@ const productsModel = require('../models/products.model');
 
 exports.index = async function (req, res, next) {
     const page = +req.query.page || 1;
-    const perPage = 10;
+    const perPage = 1;
+    const category = req.query.category;
+    const q = req.query.q;
+    let pageLink = "";
+    let currentCategory = "";
+    let filter = {};
+
+    if (category == 'all') {
+        currentCategory = 'all'
+    }
+    else if (category) {
+        filter.title = category;
+        currentCategory = category;
+        pageLink += "&category=" + category;
+    }
+
+    if (q) {
+        filter.name = new RegExp(q, 'i');
+        pageLink += "&q=" + q;
+    }
+    console.log(pageLink);
+
     // get books from models
-    const productsCursor = await productsModel.list(page, perPage);
+    const productsCursor = await productsModel.list(filter, page, perPage);
     const products = await productsCursor.toArray();
 
+    // get all category
+    const allProducts = await productsModel.getAll();
+    var categories = [];
+    for (var i = 0; i < allProducts.length; i++) {
+        categories.push(allProducts[i].title);
+    }
+    categories = [...new Set(categories)];
+
+    // Lấy các thông tin cho pagination
     let hasNextPage, hasPrevPage;
     hasPrevPage = page > 1 ? true : false;
-    if (await productsCursor.count() > ((page - 1) * perPage + products.length) )
+    if (await productsCursor.count() > ((page - 1) * perPage + products.length))
         hasNextPage = true;
-    else 
+    else
         hasNextPage = false;
-    
+
     // render page list
-    res.render('index', { 
+    res.render('index', {
         title: 'Dashboard',
+        currentCategory: category,
+        categories,
         products: products,
         hasNextPage,
         hasPrevPage,
-        currentPage: page
+        currentPage: page,
+        pageLink
     });
 
 }
@@ -56,7 +89,7 @@ exports.updateProductRender = async function (req, res, next) {
         product: product
     })
 }
-exports.updateProduct = async function (req,res,next){  
+exports.updateProduct = async function (req, res, next) {
     const id = req.body.id;
     const cover = req.body.cover;
     const title = req.body.title;
@@ -64,6 +97,6 @@ exports.updateProduct = async function (req,res,next){
     const imgs = req.body.imgs;
 
     // console.log(id, cover, title, basePrice, imgs)
-    await productsModel.updateProduct(id,cover,title,basePrice,imgs)
+    await productsModel.updateProduct(id, cover, title, basePrice, imgs)
     res.redirect('/');
 }
